544d3faea3651fcd3299d3ed5f0e3be979a83386
Tihana Krajtmajer <71774763+tkrajtmajer@users.noreply.github.com>
2025-08-03 01:21:24 +0200

    Add main and pause menus

--- Assets/Scripts/Menus/InputSystem.cs
+++ Assets/Scripts/Menus/InputSystem.cs
@@ -1,35 +1,45 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class InputSystem : MonoBehaviour
 {
     public GameObject HUDCanvas;
     public GameObject pauseCanvas;
+    public GameObject pauseContents;
     public GameObject mapCanvas;
     public GameObject mapContents; 
     public Animator scrollAnimator;
+    public Animator bookAnimator;
 
     private CanvasGroup cg;
+    private CanvasGroup cgPause;
 
     public float animationDuration = 1f;
 
     private bool isMapOpen = false;
     private bool isTransitioning = false;
 
+    private bool isPauseOpen = false;
+    private bool isTransitioningPause = false;
+    public float animationDurationPause = 1.1f;
+
     void Start()
     {
         HUDCanvas.SetActive(true);
 
         cg = mapCanvas.GetComponent<CanvasGroup>();
-        // mapCanvas.SetActive(false);
+        cgPause = pauseCanvas.GetComponent<CanvasGroup>();
         cg.alpha = 0;
+        cgPause.alpha = 0;
         mapContents.SetActive(false);
+        pauseContents.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M) && !isTransitioning)
+        if (Input.GetKeyDown(KeyCode.M) && !isTransitioning && !isPauseOpen)
         {
             if (!isMapOpen)
             {
@@ -38,6 +48,18 @@ public class InputSystem : MonoBehaviour
             else
             {
                 CloseMap();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && !isTransitioningPause && !isMapOpen)
+        {
+            if (!isPauseOpen)
+            {
+                Pause();
+            }
+            else
+            {
+                Unpause();
             }
         }
     }
@@ -82,5 +104,55 @@ public class InputSystem : MonoBehaviour
         cg.alpha = 0;
         HUDCanvas.SetActive(true);
         isTransitioning = false;
+    }
+
+    System.Collections.IEnumerator ShowContentsAfterDelayPause()
+    {
+        yield return new WaitForSecondsRealtime(animationDurationPause);
+        pauseContents.SetActive(true);
+        isTransitioningPause = false;
+        Time.timeScale = 0f; // Pause game
+    }
+
+    System.Collections.IEnumerator HideCanvasAfterDelayPause()
+    {
+        yield return new WaitForSecondsRealtime(animationDurationPause);
+        cgPause.alpha = 0;
+        HUDCanvas.SetActive(true);
+        isTransitioningPause = false;
+    }
+
+    void Pause() {
+        isTransitioningPause = true;
+        // mapCanvas.SetActive(true);
+        cgPause.alpha = 1;
+        HUDCanvas.SetActive(false);
+        bookAnimator.SetBool("isOpen", true);
+
+        // Delay showing contents until after the animation
+        StartCoroutine(ShowContentsAfterDelayPause());
+        isPauseOpen = true;
+    }
+
+    public void Unpause() {
+        isTransitioningPause = true;
+        bookAnimator.SetBool("isOpen", false);
+        pauseContents.SetActive(false);
+
+        Time.timeScale = 1f; 
+        // Delay hiding canvas & resuming game
+        StartCoroutine(HideCanvasAfterDelayPause());
+        isPauseOpen = false;
+    }
+
+    public void LoadMenu() {
+        Debug.Log("load menu");
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene("MainMenu"); 
+    }
+
+    public void QuitGame() {
+        Debug.Log("quit game");
+        Application.Quit();
     }
 }
